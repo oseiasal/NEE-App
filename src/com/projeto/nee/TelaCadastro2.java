0693466e40225e224201bf1b5f0ba9446eece86d
Oseias <Oseiasanascimento@hotmail.com>
2018-10-21 13:22:49 -0300

    Add: Tela de Cadastro2

---
+++ src/com/projeto/nee/TelaCadastro2.java
@@ -0,0 +1,14 @@
+package com.projeto.nee;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TelaCadastro2 extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastro2);
+	}
+
+}
